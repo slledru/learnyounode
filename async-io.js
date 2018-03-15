8fb5f331699841ea8a202955ede36ec026c02a23
slledru <slledru@msn.com>
2018-03-15 17:20:21 -0600

    async i/o completed

---
+++ async-io.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+if (process.argv.length > 2) {
+  const filename = process.argv[2]
+  const buffer = fs.readFile(filename, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const fileContent = data.toString().match(/\n/g)
+      console.log(fileContent.length);
+    }
+  })
+} else {
+  console.log('0');
+}
