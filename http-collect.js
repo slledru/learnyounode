dfb6354f748c0628e9c207c362409246dcbbee15
slledru <slledru@msn.com>
2018-03-15 19:57:13 -0600

    use bl

--- http-collect.js
+++ http-collect.js
@@ -1,21 +1,17 @@
 const http = require('http')
+const bl = require('bl')
 
 if (process.argv.length > 2) {
   const url = process.argv[2]
-  const array = []
-  let total = 0
   http.get(url, (response) => {
-    response.on('data', (data) => {
-      const str = data.toString()
-      total += str.length
-      array.push(str)
-    })
-    response.on('error', (error) => {
-      console.log('error', error);
-    })
-    response.on('end', () => {
-      console.log(total)
-      console.log(array.join(''))
-    })
+    response.pipe(bl((error, data) => {
+      if (error) {
+        console.log(error)
+        return
+      }
+      const text = data.toString()
+      console.log(text.length)
+      console.log(text)
+    }))
   })
 }
